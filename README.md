e7d834845b16e8219b65d34b520aabff48ba08d2
Carlos Espiritu <carlosespiritu.pe@gmail.com>
2021-02-25 20:24:00 -0500

    Configure and implement Spring REST Docs for Greeting service.

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # RESTful Web Service with Spring Boot
 
 Demo project using Spring Boot Web and Spring Boot Actuator.
+
+## Sprint REST Docs
+
+Helps you produce documentation for your RESTful services using Asciidoctor by default. 
+The Asciidoctor file is then processed to produce an HTML file with the complete 
+documentation based on the tests created for our services.
+
+Uses snippets produced by test written with Spring MVC's [test framework][1], 
+Spring WebFlux's [WebTestClient][2] or [REST Assured 3][3].
+
+### Requirements
+
+- Java 8
+- Spring 5.0.2 or later
+
+The `spring-restdocs-restassured` module requires REST Assured 3.0.
+
+Provides first-class support for JUnit 4 and JUnit 5. Other frameworks, 
+such as TestNG, are also supported, but require more setup.
+
+[1]: https://docs.spring.io/spring-framework/docs/5.0.x/spring-framework-reference/testing.html#spring-mvc-test-framework
+[2]: https://docs.spring.io/spring-framework/docs/5.0.x/spring-framework-reference/testing.html#webtestclient
+[3]: https://rest-assured.io
+
+### Snippets
+
+You must create an `.adoc` source file first. You can then include 
+the generated snippets in the Asciidoc file created.
+
+The default location of the source files, and the resulting HTML files depends on the build tool used:
+
+|Build Tool | Source files               | Generated files               |
+|-----------|:--------------------------:|------------------------------:|
+|Maven      | `src/main/asciidoc/*.adoc` | `target/generated-docs/*.html`|
+|Gradle     | `src/docs/asciidoc/*.adoc` | `build/asciidoc/html5/*.html` |
+
--- src/test/java/pe/carlosesp/demo/demorestservice/controller/GreetingControllerTest.java
+++ src/test/java/pe/carlosesp/demo/demorestservice/controller/GreetingControllerTest.java
@@ -1,19 +1,32 @@
 package pe.carlosesp.demo.demorestservice.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import pe.carlosesp.demo.demorestservice.domain.Greeting;
 import pe.carlosesp.demo.demorestservice.service.GreetingService;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(GreetingController.class)
 public class GreetingControllerTest {
 
@@ -23,16 +36,28 @@ public class GreetingControllerTest {
     @MockBean
     private GreetingService service;
 
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext,
+                      RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
     @Test
     public void getGreetingWithoutName_ShouldReturnDefaultGreeting() throws Exception {
         Greeting greeting = new Greeting(1L, "Hello, World!");
         given(service.getGreeting(anyString())).willReturn(greeting);
 
-        this.mockMvc.perform(get("/greeting"))
+        this.mockMvc.perform(get("/greeting").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("id").exists())
-                .andExpect(jsonPath("content").value("Hello, World!"));
+                .andExpect(jsonPath("content").value("Hello, World!"))
+                .andDo(document("greeting", responseFields(
+                        fieldWithPath("id").description("Id of the request"),
+                        fieldWithPath("content").description("Greeting content")
+                )));
     }
 
     @Test
@@ -44,7 +69,15 @@ public class GreetingControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("id").exists())
-                .andExpect(jsonPath("content").value("Hello, Carlos!"));
+                .andExpect(jsonPath("content").value("Hello, Carlos!"))
+                .andDo(document("greeting-param",
+                        requestParameters(
+                                parameterWithName("name").description("The name to include in the greeting").optional()
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Id of the request"),
+                                fieldWithPath("content").description("Greeting content"))
+                ));
     }
 
 }
